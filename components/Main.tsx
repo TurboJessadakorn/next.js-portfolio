0df090526f2430d71c50ae6da807a59ceae27256
TURBO <jessadakorn.paopati@gmail.com>
2024-01-21 04:19:45 +0700

    Edit link

--- components/Main.tsx
+++ components/Main.tsx
@@ -21,7 +21,7 @@ const Main = () => {
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <Link
-              href='https:www.linkedin.com/in/jessadakorn-paopatimakorn'
+              href='https://linkedin.com/in/jessadakorn-paopatimakorn'
               target='_blank'
               rel='noreferrer'
             >
